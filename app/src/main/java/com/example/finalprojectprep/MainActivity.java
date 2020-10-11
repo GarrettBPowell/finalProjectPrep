134f0b66be6376684dd9bf1d7d21573bd4121b0f
Garrett Powell <garettbpowell@gmail.com>
2020-10-11 13:02:42 -0500

    we shmoovin

--- app/src/main/java/com/example/finalprojectprep/MainActivity.java
+++ app/src/main/java/com/example/finalprojectprep/MainActivity.java
@@ -10,6 +10,9 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.lang.reflect.Array;
+import java.time.DayOfWeek;
+import java.util.ArrayList;
 import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
@@ -30,8 +33,9 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        HashMap days = new HashMap();
-        HashMap events = new HashMap();
+        final HashMap<String, ArrayList<calData>> DAY = new HashMap();
+        final ArrayList<calData> EVENTS = new ArrayList<>();
+
         final EditText time = findViewById(R.id.editTextTime);
         final EditText time2 = findViewById(R.id.editTextTime2);
         final EditText date = findViewById(R.id.editTextDate);
@@ -63,19 +67,60 @@ public class MainActivity extends AppCompatActivity {
         //back to normal event button listener
         backToNormalView.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                if(backToNormalView.getText().equals("Create Event"))
-                {
-                    time.setVisibility(View.INVISIBLE);
-                    time2.setVisibility(View.INVISIBLE);
-                    date.setVisibility(View.INVISIBLE);
-                    content.setVisibility(View.INVISIBLE);
+                if(backToNormalView.getText().equals("Create Event")) {
 
+                   if(time.getText().toString().equals("") || time2.getText().toString().equals("") || date.getText().toString().equals("") || content.getText().toString().equals("")) {
+
+                       Toast.makeText(getApplicationContext(),"Please Fill All Fields", Toast.LENGTH_LONG).show();
+                   }
+                   else {
+                       //break date up
+                       String[] breakDate = date.getText().toString().split("/");
+                       //create new Object
+                       calData newEvent = new calData((breakDate[0]), breakDate[1], breakDate[2], time.getText().toString(), time2.getText().toString(), content.getText().toString());
+
+                       //events contains the object which allows multiple events to exist on the same day at the same time but have differing content
+                       EVENTS.add(newEvent);
+                       //day contains the event map so it should contain different instances of EVENT with different day keys so the data for certain days can be looked up
+                       DAY.put(newEvent.getMonth() + newEvent.getDay() + newEvent.getYear(), EVENTS);
+
+
+                       time.setVisibility(View.INVISIBLE);
+                       time2.setVisibility(View.INVISIBLE);
+                       date.setVisibility(View.INVISIBLE);
+                       content.setVisibility(View.INVISIBLE);
+
+                       addEvent.setVisibility(View.VISIBLE);
+                       backToNormalView.setVisibility((View.INVISIBLE));
+                       removeEvent.setVisibility(View.VISIBLE);
+                       lookUpDay.setVisibility(View.VISIBLE);
+                   }
+                }
+                else if(backToNormalView.getText().equals("")) {
+                    //stuff
+
+                    addEvent.setVisibility(View.VISIBLE);
+                    backToNormalView.setVisibility((View.INVISIBLE));
+                    removeEvent.setVisibility(View.VISIBLE);
+                    lookUpDay.setVisibility(View.VISIBLE);
+                }
+                else if(backToNormalView.getText().equals("i")) {
+                                   //stuff
+
+                    addEvent.setVisibility(View.VISIBLE);
+                    backToNormalView.setVisibility((View.INVISIBLE));
+                    removeEvent.setVisibility(View.VISIBLE);
+                    lookUpDay.setVisibility(View.VISIBLE);
+                }
+                else {
+                    addEvent.setVisibility(View.VISIBLE);
+                    backToNormalView.setVisibility((View.INVISIBLE));
+                    removeEvent.setVisibility(View.VISIBLE);
+                    lookUpDay.setVisibility(View.VISIBLE);
                 }
 
-                addEvent.setVisibility(View.VISIBLE);
-                backToNormalView.setVisibility((View.INVISIBLE));
-                removeEvent.setVisibility(View.VISIBLE);
-                lookUpDay.setVisibility(View.VISIBLE);
+
+
             };
         });
 
--- app/src/main/java/com/example/finalprojectprep/calData.java
+++ app/src/main/java/com/example/finalprojectprep/calData.java
@@ -2,29 +2,37 @@ package com.example.finalprojectprep;
 
 public class calData
 {
-    private int year;
-    private int month;
-    private int day;
-    private String time;
+    private String year;
+    private String month;
+    private String day;
+    private String timeStart;
     private String timeOfDay;
+    private String timeEnd;
+    private String content;
 
-    calData(int y, int m, int d, String t)
+    calData(String m, String d, String y, String ts, String te, String con)
     {
         year = y;
         month = m;
         day = d;
-        time = t;
+        timeStart = ts;
+        timeEnd = te;
+        content = con;
     }
 
-    public void setYear(int y){ year = y;};
-    public void setMonth(int m){ month = m;};
-    public void setDay(int d){ day = d;};
-    public void setHour(String t){ time = t;};
+    public void setYear(String y){ year = y;};
+    public void setMonth(String m){ month = m;};
+    public void setDay(String d){ day = d;};
+    public void setTimeStart(String ts){ timeStart = ts;};
+    public void setTimeEnd(String te){timeEnd = te;};
     public void setTimeOfDay(String tod){ timeOfDay = tod;};
+    public void setContent(String con){content = con;};
 
-    public int getYear(){ return year;};
-    public int getMonth(){ return month;};
-    public int getDay(){ return day;};
-    public String getHour(){ return time;};
+    public String getYear(){ return year;};
+    public String getMonth(){ return month;};
+    public String getDay(){ return day;};
+    public String getTimeStart(){ return timeStart;};
+    public String getTimeEnd(){ return timeEnd;};
     public String getTimeOfDay(){ return timeOfDay;};
+    public String getContent(){return content;};
 }
