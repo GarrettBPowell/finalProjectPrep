4df9a1316d7c3f55744e8f5ddbca01bb0dc9680b
Garrett Powell <garettbpowell@gmail.com>
2020-10-10 21:34:19 -0500

    layout changes

--- app/src/main/java/com/example/finalprojectprep/MainActivity.java
+++ app/src/main/java/com/example/finalprojectprep/MainActivity.java
@@ -6,11 +6,11 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CalendarView;
-import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
-import java.util.Calendar;
+import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -30,17 +30,75 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        final Button button = findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
+        HashMap days = new HashMap();
+        HashMap events = new HashMap();
+        final EditText time = findViewById(R.id.editTextTime);
+        final EditText time2 = findViewById(R.id.editTextTime2);
+        final EditText date = findViewById(R.id.editTextDate);
+        final EditText content = findViewById(R.id.editTextTextMultiLine);
+
+
+        final Button addEvent = findViewById(R.id.addEvent);
+        final Button backToNormalView = findViewById(R.id.backToNormalView);
+        final Button removeEvent = findViewById(R.id.removeEvent);
+        final Button lookUpDay = findViewById(R.id.lookUpDay);
+
+        //add event button listener
+        addEvent.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                EditText time = findViewById(R.id.editTextTime);
-                EditText date = findViewById(R.id.editTextDate);
-                EditText content = findViewById(R.id.editTextTextMultiLine);
-                Button addEvent = findViewById(R.id.button);
+
                 time.setVisibility(View.VISIBLE);
+                time2.setVisibility(View.VISIBLE);
                 date.setVisibility(View.VISIBLE);
                 content.setVisibility(View.VISIBLE);
                 addEvent.setVisibility(View.INVISIBLE);
+                backToNormalView.setVisibility((View.VISIBLE));
+                removeEvent.setVisibility(View.INVISIBLE);
+                lookUpDay.setVisibility(View.INVISIBLE);
+
+
+            };
+        });
+
+        //back to normal event button listener
+        backToNormalView.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                if(backToNormalView.getText().equals("Create Event"))
+                {
+                    time.setVisibility(View.INVISIBLE);
+                    time2.setVisibility(View.INVISIBLE);
+                    date.setVisibility(View.INVISIBLE);
+                    content.setVisibility(View.INVISIBLE);
+
+                }
+
+                addEvent.setVisibility(View.VISIBLE);
+                backToNormalView.setVisibility((View.INVISIBLE));
+                removeEvent.setVisibility(View.VISIBLE);
+                lookUpDay.setVisibility(View.VISIBLE);
+            };
+        });
+
+        //remove event button listener
+        removeEvent.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                addEvent.setVisibility(View.INVISIBLE);
+                removeEvent.setVisibility(View.INVISIBLE);
+                lookUpDay.setVisibility(View.INVISIBLE);
+                backToNormalView.setText(R.string.removeEvent);
+                backToNormalView.setVisibility((View.VISIBLE));
+            };
+        });
+
+        //create event button listener
+        lookUpDay.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+                addEvent.setVisibility(View.INVISIBLE);
+                removeEvent.setVisibility(View.INVISIBLE);
+                lookUpDay.setVisibility(View.INVISIBLE);
+                backToNormalView.setText(R.string.loopkUpDay);
+                backToNormalView.setVisibility(View.VISIBLE);
             };
         });
     }
--- app/src/main/java/com/example/finalprojectprep/calData.java
+++ app/src/main/java/com/example/finalprojectprep/calData.java
@@ -5,21 +5,26 @@ public class calData
     private int year;
     private int month;
     private int day;
-    private int hour;
-    private int minute;
+    private String time;
     private String timeOfDay;
+
+    calData(int y, int m, int d, String t)
+    {
+        year = y;
+        month = m;
+        day = d;
+        time = t;
+    }
 
     public void setYear(int y){ year = y;};
     public void setMonth(int m){ month = m;};
     public void setDay(int d){ day = d;};
-    public void setHour(int h){ hour = h;};
-    public void setMinute(int m){ minute = m;};
+    public void setHour(String t){ time = t;};
     public void setTimeOfDay(String tod){ timeOfDay = tod;};
 
-    public int getYear(int y){ return year;};
-    public int getMonth(int m){ return month;};
-    public int getDay(int d){ return day;};
-    public int getHour(int h){ return hour;};
-    public int getMinute(int m){ return minute;};
-    public String getTimeOfDay(String tod){ return timeOfDay;};
+    public int getYear(){ return year;};
+    public int getMonth(){ return month;};
+    public int getDay(){ return day;};
+    public String getHour(){ return time;};
+    public String getTimeOfDay(){ return timeOfDay;};
 }
